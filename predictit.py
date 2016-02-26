60be1b8bb683b23dbc638eae3b5f17da48e61fcb
Henry Doupe <hdoupe@yahoo.com>
2016-02-25 21:05:51 -0500

    Update predictit.py

--- predictit.py
+++ predictit.py
@@ -1,17 +1,24 @@
-from selenium import webdriver
-from bs4 import BeautifulSoup
+import json,requests
 from datetime import datetime
 from sqlite_helper import Sqlite_Helper
-import time,re,sched,datetime,argparse
+import time,re,sched,datetime,argparse,json,requests
+from selenium import webdriver
+from bs4 import BeautifulSoup
 br = webdriver.PhantomJS()
 
-pathToTable = "pathto/prediction_markets.sqlite"
+pathToTable = "prediction_markets.sqlite"
 
-# earlyStates = ["https://www.predictit.org/Market/1675/Who-will-win-the-2016-Nevada-Republican-caucuses","https://www.predictit.org/Market/1882/Who-will-place-2nd-in-the-2016-Nevada-Republican-caucuses","https://www.predictit.org/Market/1674/Who-will-win-the-2016-South-Carolina-Democratic-primary"]
+superTuesday = ['TXPRMRY16.GOP',' VAPRMRY16.GOP', 'GAPRMRY16.GOP', 'MAPRMRY16.GOP', 
+	'OKPRMRY16.GOP', 'MNCAUCUS16.GOP', 'ALPRMRY16.GOP', 'TNPRMRY16.GOP','ARPRMRY16.GOP',
+	'MAPRMRY16.DEM','AKCAUCUS16.GOP', 'OKPRMRY16.DEM','VTPRMRY16.GOP', 'MNCAUCUS16.DEM', 
+	'TXPRMRY16.DEM','COCAUCUS16.DEM', 'ARPRMRY16.DEM',  'VAPRMRY16.DEM', 'GAPRMRY16.DEM',
+	'ALPRMRY16.DEM', 'TNPRMRY16.DEM','VTPRMRY16.DEM']
 
-superTuesday = ["https://www.predictit.org/Market/1716/Who-will-win-the-2016-Massachusetts-Republican-primary","https://www.predictit.org/Market/1715/Who-will-win-the-2016-Georgia-Republican-primary","https://www.predictit.org/Market/1721/Who-will-win-the-2016-Virginia-Republican-primary","https://www.predictit.org/Market/1717/Who-will-win-the-2016-Oklahoma-Republican-primary","https://www.predictit.org/Market/1709/Who-will-win-the-2016-Minnesota-Republican-caucuses","https://www.predictit.org/Market/1713/Who-will-win-the-2016-Alabama-Republican-primary","https://www.predictit.org/Market/1719/Who-will-win-the-2016-Texas-Republican-primary","https://www.predictit.org/Market/1718/Who-will-win-the-2016-Tennessee-Republican-primary","https://www.predictit.org/Market/1714/Who-will-win-the-2016-Arkansas-Republican-primary","https://www.predictit.org/Market/1712/Who-will-win-the-2016-Alaska-Republican-caucuses","https://www.predictit.org/Market/1720/Who-will-win-the-2016-Vermont-Republican-primary","https://www.predictit.org/Market/1710/Who-will-win-the-2016-Minnesota-Democratic-caucuses","https://www.predictit.org/Market/1723/Who-will-win-the-2016-Arkansas-Democratic-primary","https://www.predictit.org/Market/1725/Who-will-win-the-2016-Massachusetts-Democratic-primary","https://www.predictit.org/Market/1726/Who-will-win-the-2016-Oklahoma-Democratic-primary","https://www.predictit.org/Market/1728/Who-will-win-the-2016-Texas-Democratic-primary","https://www.predictit.org/Market/1730/Who-will-win-the-2016-Virginia-Democratic-primary","https://www.predictit.org/Market/1722/Who-will-win-the-2016-Alabama-Democratic-primary","https://www.predictit.org/Market/1711/Who-will-win-the-2016-Colorado-Democratic-caucuses","https://www.predictit.org/Market/1727/Who-will-win-the-2016-Tennessee-Democratic-primary","https://www.predictit.org/Market/1724/Who-will-win-the-2016-Georgia-Democratic-primary","https://www.predictit.org/Market/1729/Who-will-win-the-2016-Vermont-Democratic-primary","https://www.predictit.org/Market/1912/Who-will-win-the-Louisiana-Republican-primary","https://www.predictit.org/Market/1911/Who-will-win-the-Louisiana-Democratic-primary","https://www.predictit.org/Market/1910/Who-will-win-the-Nebraska-Democratic-caucuses"]
+national = ['RNOM16','USPREZ16', 'USPREZ16','PREZPRTY16', 'RVP16', 'DVP16',
+	'CARSON.DROPOUT.022916', 'WOMAN.USPREZ16','KASICH.DROPOUT.022916','BLOOMBERG.RUN.MAR',
+	'370.REPPREZ16','TRUMP.INDY.2016','GOPBROKERED.2016','370.DEMPREZ16','ROMNEY.RUN.2016',
+	'CARSON.INDY.2016']
 
-national = ["https://www.predictit.org/Market/1233/Who-will-win-the-2016-Republican-presidential-nomination","https://www.predictit.org/Market/1234/Who-will-win-the-2016-US-presidential-election","https://www.predictit.org/Market/1232/Who-will-win-the-2016-Democratic-presidential-nomination","https://www.predictit.org/Market/1296/Which-party-will-win-the-2016-US-Presidential-election","https://www.predictit.org/Market/1529/Who-will-win-the-2016-Republican-vice-presidential-nomination","https://www.predictit.org/Market/1530/Who-will-win-the-2016-Democratic-vice-presidential-nomination"]
 
 def cleanUp(junk):
     if not junk:
@@ -19,87 +26,31 @@ def cleanUp(junk):
     x = str(junk).encode('ascii','ignore')
     return re.sub('\n|\t/|\r',' ',x).strip()
 
-def getSource(url,br, sleep = 2):
-    br.get(url)
-    time.sleep(sleep)
-    src = br.page_source.encode('ascii','ignore')
-    return src
-SCURL = "https://www.predictit.org/Market/1673/Who-will-win-the-2016-South-Carolina-Republican-primary"
 
-"""
-	Gets the categories for the Political side of the betting market
-"""
-def getCategories():
-	url = "https://www.predictit.org/Home/browseonlycategories?categoryid=6"
-	soup = BeautifulSoup(getSource(url,br), "html.parser")
-	div = soup.find('div', {'class':'overflow'})
-	base = "https://www.predictit.org/"
-	categories = []
-	for a in div.find_all('a'):
-		categories.append(base + str(a.get('href').encode('ascii','ignore')))
+def getContracts(tickersymbol):
+	url = "https://www.predictit.org/api/marketdata/ticker/{ts}".format(ts = tickersymbol)
+	response = requests.get(url)
+	contracts = response.json()
 	
-	return categories
-
-
-""" 
-	Having trouble finding the contract list for this category. It should in either 
-	'id':marketList or 'class':row but it's not showing up
-"""
-def getContests(category = "https://www.predictit.org/Browse/Group/54"):
-	soup = BeautifulSoup(getSource(category,br),"html.parser")
-	marketList = soup.find('div',{'id','marketList'})
-	print marketList
-	marketList = soup.find('div', {'class','row'})
-	print '2',marketList
-	for div in marketList.find_all('div'):
-		print div
-
-
-# def run():
-# 	categories = getCategories()
-# 	
-# 	for category in categories:
-# 		contests = getContests(category)
-# 		for contest in contests:
-#			scrapeContest(contest)
-# 
-# 	return someData
-
-
-"""
-	Returns dictionary with keys: contest, question, buy, sell (buy and sell are dicts
-	with keys yes no
-"""
-def scrapeContest(url = SCURL):
-	soup = BeautifulSoup(getSource(url,br), "html.parser")
-	td = soup.find('td',{'class':'sMarket'})
+	if contracts == None:
+		print "NONE",tickersymbol
+		return None
+	
+	contest = contracts['Name'].encode('ascii')
+	
 	results = []
-	contest = soup.find('h3',{'style':'margin-top: 1px;'})
-	contest = contest.get_text()
-	for table in td.find_all('table',{'class':'sharesMarket'}):
-		if table:
-			trs = table.find_all('tr')
-			tr = trs[1]
-			
-			data = {'CONTEST':contest}		
-			
-			div = table.find('div',{'class':'outcome'})
-			a = div.find('a')
-			question = a.get('title')
-			data['QUESTION'] = question
-			
-			sharesUps = tr.find_all('span',{'class':'sharesUp'})
-			sharesDowns = tr.find_all('span',{'class':'sharesDown'})
-			data['BUY_YES'],data['BUY_NO'] = sharesUps[0].get_text(),sharesDowns[0].get_text()
-			data['SELL_YES'],data['SELL_NO'] = sharesUps[1].get_text(),sharesUps[1].get_text()
-			
-			for key in ['BUY_YES','BUY_NO','SELL_YES','SELL_NO']:
-				if data[key] == 'None':
-					data[key] = u'0'
-			
-			results.append(data)
-	del soup,td
-
+	
+	for contract in contracts['Contracts']:
+		data = {}
+		data['CONTEST'] = contest
+		data['QUESTION'] = contract['LongName'].encode('ascii')
+		data['BUY_YES'] = str(int((contract['BestBuyYesCost'] if contract['BestBuyYesCost'] else 0) * 100))
+		data['BUY_NO'] = str(int((contract['BestBuyNoCost']  if contract['BestBuyNoCost'] else 0)* 100))
+		data['SELL_YES'] = str(int((contract['BestSellYesCost']  if contract['BestSellYesCost'] else 0)*100))
+		data['SELL_NO'] = str(int((contract['BestSellNoCost'] if contract['BestSellNoCost'] else 0)*100))
+		
+		results.append(data)
+	del contracts
 	return results
 
 
@@ -116,9 +67,9 @@ def go():
 	DATE = SH.cursor.fetchall()[0][0]
 	print DATE
 	keys = ['CONTEST','QUESTION','BUY_YES', 'BUY_NO', 'SELL_YES','SELL_NO']
-	for link in national+superTuesday:
+	for tickersymbol in national + superTuesday + ['SCPRMRY16.DEM']:
 		try:
-			results = scrapeContest(link)
+			results = getContracts(tickersymbol)
 			rows = []
 			for result in results:
 				row = []
@@ -133,7 +84,7 @@ def go():
 			del results,rows
 		except Exception as e:
 			import traceback
-			print 'exception',link,e,traceback.print_exc()
+			print 'exception',tickersymbol,e,traceback.print_exc()
 			continue
 	SH.connection.close()
 
